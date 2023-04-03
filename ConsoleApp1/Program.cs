fd82cdfe4b6606ba96bd26743589944ecf738d6f
ilya karpov <ilia.karpov@gmail.com>
2023-04-03 12:54:17 +0200

    add repro example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+
+var endpoint = "";
+var authKey = "";
+var databaseId = "";
+var containerId = "";
+
+var client = new CosmosClientBuilder(endpoint, authKey)
+    .WithSerializerOptions(new CosmosSerializationOptions
+    {
+        PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+    })
+    .Build();
+
+var database = client.GetDatabase(databaseId);
+await database.CreateContainerIfNotExistsAsync(new ContainerProperties(containerId, "/documentType"));
+
+var container = database.GetContainer(containerId);
+await container.DeleteContainerAsync();
